b4e92c1d604ba0daadf72e3991b83cb829aa329a
Verwegen,Daniell,D <d.verwegen@student.fontys.nl>
2024-03-08 00:25:29 +0100

    style - readability, text added & divider added

--- lib/main.dart
+++ lib/main.dart
@@ -85,6 +85,7 @@ class _AudioPlayerPageState extends State<AudioPlayerPage> {
           centerTitle: true,
         ),
         body: SingleChildScrollView(
+          padding: const EdgeInsets.only(bottom: 50.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
@@ -136,12 +137,25 @@ class _AudioPlayerPageState extends State<AudioPlayerPage> {
                   onPressed: togglePlayPause,
                 ),
               ),
-              const SizedBox(
-                  height: 20), // Ruimte toevoegen onder de afspeelknop
-              const Text(
-                'Geniet van het verhaal!', // Tekst toegevoegd onder de knop
-                style: TextStyle(
-                  fontSize: 18,
+              const SizedBox(height: 20),
+              Divider(
+                thickness: 2.0,
+                indent: 20.0,
+                endIndent: 20.0,
+                color: Colors.grey[400],
+              ),
+              SizedBox(height: 20),
+              const Padding(
+                padding:
+                    EdgeInsets.all(16.0), // De gewenste horizontale padding.
+                child: Text(
+                  """Er waren eens een koning en een koningin. Zij hadden een grote wens. Ze wilden heel graag een kindje. 
+
+Op een mooie dag, toen alle rozen van het paleis bloeiden, ging de wens in vervulling. Er werd een prachtig prinsesje geboren. In het hele land was het feest en iedereen mocht naar het paleis komen. Ook alle goede feeën. Helaas was de koningin één fee vergeten uit te nodigen. Toen het feest in volle gang was, net op het moment dat de feeën hun wensen uitspraken voor het prinsesje, sloegen met een klap de paleisdeuren open. Daar was de vergeten fee. “Ik heb ook nog een wens voor het prinsesje. Op de dag dat ze achttien jaar wordt, zal ze zich prikken aan een spinnewiel en sterven.” “Nee! Dat mag niet gebeuren”, sprak een kleine fee. “Ik heb mijn wens nog niet uitgesproken. De prinses zal niet sterven, maar honderd jaar slapen.” En zo gebeurde het. 
+
+Op de dag van haar achttiende verjaardag zwierf de prinses rond in het paleis. In een ver kamertje, hoog in de toren vond zij een spinnewiel. Ze prikte zich... en viel in een diepe slaap. Als door een wonder viel iedereen in het paleis samen met haar in slaap en groeide er een dikke rozenhaag rond het kasteel. Alle mensen in het land vergaten het kasteel en de koninklijke familie. Tot op een dag - het was precies honderd jaar later - er een mooie prins voorbij de enorme rozenstruik kwam…""",
+                  style: TextStyle(fontSize: 18, fontStyle: FontStyle.italic),
+                  textAlign: TextAlign.left,
                 ),
               ),
             ],
